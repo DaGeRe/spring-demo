4f70249a3dda2439b12b4ceac4b65c5525fb944b
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-04-09 16:46:01 +0200

    Add kieker spring probes, adapt DemoApplication to load it by using spring-config.xml

--- src/main/java/net/kiekertest/demo/DemoApplication.java
+++ src/main/java/net/kiekertest/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package net.kiekertest.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public static void main(final String[] args) {
+		System.out.println("Starting spring boot");
+		final ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("config/spring-config.xml");
+
+		applicationContext.start();
+
+		final SpringApplication build = new SpringApplicationBuilder(applicationContext, DemoApplication.class).build();
+		build.run(args);
+
+		// SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
